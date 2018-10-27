86b9bb6792f0ab1e464d17c243fa547ffa796288
jwin <jwin1968@gmail.com>
2018-10-27 17:58:43 +0200

    daemon test Ok

--- TODO.md
+++ TODO.md
@@ -1,11 +1,6 @@
+https://github.com/JetBrains/intellij-sdk-docs/tree/master/code_samples/plugin_sample
+
 
 - start haskell-tools daemon-process *automatically* with default port 4123
 `stack ht-daemon`
 
-- where to open/close socket + channels ?
-
-```
-socket.close()
-println("socket CLOSED")
-```
---> https://github.com/JetBrains/intellij-sdk-docs/tree/master/code_samples/plugin_sample
--- src/main/kotlin/ExtractBindingAction.kt
+++ src/main/kotlin/ExtractBindingAction.kt
@@ -1,11 +1,29 @@
+import com.google.gson.GsonBuilder
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.io.readUTF8Line
 import kotlinx.coroutines.io.writeStringUtf8
 import kotlinx.coroutines.launch
 
 class ExtractBindingAction : AnAction("ht ExtractBinding") {
+
+    data class AddPackagesCommand(
+        val tag: String = "AddPackages",
+        val addedPathes: List<String>
+    )
+    data class RefactoringCommand(
+        val tag: String = "PerformRefactoring",
+        val refactoring: String,
+        val modulePath: String,
+        val editorSelection: String,
+        val details: List<String>,
+        val shutdownAfter: Boolean = false,
+        val diffMode: Boolean = true
+    )
+
+    private val gson = GsonBuilder().disableHtmlEscaping().create()
 
     @KtorExperimentalAPI
     override fun actionPerformed(event: AnActionEvent) {
@@ -13,9 +31,18 @@ class ExtractBindingAction : AnAction("ht ExtractBinding") {
         GlobalScope.launch {
 
             // TODO get editor's selected range and user-input for the name
-            HtProjectComponentImpl.output.writeStringUtf8("ExtractBinding DepTree.hs 31:23-31:60 name\r\n")
-            val response = HtProjectComponentImpl.input.readUTF8Line(1000)
-            println("Server said: '$response'")
+
+/* TODO only add packages once !?
+            val addPackagesCommand = AddPackagesCommand(addedPathes = listOf("/home/jwin/IdeaProjects/plugin-test/src"))
+            HtProjectComponentImpl.output.writeStringUtf8(gson.toJson(addPackagesCommand))
+
+            println("Server said: '${HtProjectComponentImpl.input.readUTF8Line()}'")
+*/
+
+            val refactoringCommand = RefactoringCommand(refactoring = "ExtractBinding", editorSelection = "31:23-31:60", modulePath = "/home/jwin/IdeaProjects/plugin-test/src/DepTree.hs", details = listOf("nameABC"))
+            HtProjectComponentImpl.output.writeStringUtf8(gson.toJson(refactoringCommand))
+
+            println("Server said: '${HtProjectComponentImpl.input.readUTF8Line()}'")
             // TODO get diff / change document etc.
 
         }
